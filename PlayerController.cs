0b35ee47efb9b08d01db87e8242319f3ced094bd
Joshua Capstick <55792213+misterpilgrim@users.noreply.github.com>
2020-02-17 15:10:16 -0400

    Wall Detection
    Now has a proper way of handling wall detection. Using the WallCheck() method, a raycast is deployed every time the player attempts to move and detects whether or not a wall is in front of the player. If an object with the tag "Wall" is in the desired direction of movement, the player will not move. (Also made some minor tweaks/adjustments to neatness + orderliness of code)

--- PlayerController.cs
+++ PlayerController.cs
@@ -21,7 +21,6 @@ public class PlayerController : MonoBehaviour
         compass = Compass.North;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, target, movespeed * Time.deltaTime);
@@ -30,22 +29,22 @@ public class PlayerController : MonoBehaviour
         TurnCheck();
         StepCheck();
 
-        if (Input.GetKey(KeyCode.W)) // Forward
+        if (Input.GetKey(KeyCode.W)) // forward
         {
             Debug.Log("Walked FORWARD");
             Step("Forward");
         }
-        else if (Input.GetKeyDown(KeyCode.A)) // Left Turn
+        else if (Input.GetKeyDown(KeyCode.A)) // left turn
         {
             Debug.Log("Turned LEFT");
             Turn("Left");
         }
-        else if (Input.GetKey(KeyCode.S)) // Backward
+        else if (Input.GetKey(KeyCode.S)) // backward
         {
             Debug.Log("Walked BACKWARD");
             Step("Backward");
         }
-        else if (Input.GetKeyDown(KeyCode.D)) // Right Turn
+        else if (Input.GetKeyDown(KeyCode.D)) // right turn
         {
             Debug.Log("Turned RIGHT");
             Turn("Right");
@@ -55,45 +54,79 @@ public class PlayerController : MonoBehaviour
     // determines which direction player takes forward/backward step based off current direction (N/S/E/W)
     void Step(string direction)
     {
+        // handles forward movements
         if (direction == "Forward" && moving == false && turning == false)
         {
             switch (compass)
             {
                 case Compass.North:
+                    if (WallCheck(Vector3.forward))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x, transform.position.y, transform.position.z + distance);
                     break;
 
                 case Compass.South:
+                    if (WallCheck(Vector3.back))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x, transform.position.y, transform.position.z - distance);
                     break;
 
                 case Compass.East:
+                    if (WallCheck(Vector3.right))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
                     break;
 
                 case Compass.West:
+                    if (WallCheck(Vector3.left))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x - distance, transform.position.y, transform.position.z);
                     break;
             }
         }
 
+        // handles backward movements
         else if (direction == "Backward" && moving == false && turning == false)
         {
             switch (compass)
             {
                 case Compass.North:
+                    if (WallCheck(Vector3.back))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x, transform.position.y, transform.position.z - distance);
                     break;
 
                 case Compass.South:
+                    if (WallCheck(Vector3.forward))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x, transform.position.y, transform.position.z + distance);
                     break;
 
                 case Compass.East:
+                    if (WallCheck(Vector3.left))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x - distance, transform.position.y, transform.position.z);
                     break;
 
                 case Compass.West:
+                    if (WallCheck(Vector3.right))
+                    {
+                        break;
+                    }
                     target = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
                     break;
             }
@@ -103,7 +136,7 @@ public class PlayerController : MonoBehaviour
     // determines which direction player faces on left/right turn based off current direction (N/S/E/W)
     void Turn(string direction)
     {
-        // Left Turn
+        // handles left turns
         if (direction == "Left" && turning == false && moving == false)
         {
             switch (compass)
@@ -134,7 +167,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // Right Turn
+        // handles right turns
         else if (direction == "Right" && turning == false && moving == false)
         {
             switch (compass)
@@ -164,6 +197,22 @@ public class PlayerController : MonoBehaviour
                     break;
             }
         }
+    }
+
+    // determines whether or not there is a wall in front of player, and how to react
+    bool WallCheck(Vector3 direction)
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, direction, out hit, distance))
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                Debug.Log("Wall hit");
+                return true;
+            }
+        }
+        return false;
     }
 
     // true/false determination on whether player is currently walking
