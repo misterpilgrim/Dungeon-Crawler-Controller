5aea511ce96fc6e79ae0e7022e61f293fd6476a1
Joshua Capstick <55792213+misterpilgrim@users.noreply.github.com>
2020-03-04 20:23:56 -0400

    Camera Control
    To be applied to a child camera within the main object using the Character Controller script. Provides more dynamic camera view/movement than the standard stiff and static-like one.

---
+++ CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //private Vector2 cameraDirection;
+    Vector2 cameraDirection;
+    private float min = -20;
+    private float max = 20;
+    private float rotationX = 0;
+    private float rotationY = 0f;
+    public float sensitivity;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        rotationY += Input.GetAxisRaw("Mouse X") * sensitivity;
+        rotationX += Input.GetAxisRaw("Mouse Y") * sensitivity;
+
+        rotationY = Mathf.Clamp(rotationY, min, max);
+        rotationX = Mathf.Clamp(rotationX, min, max);
+
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+}
